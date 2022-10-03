5a6de126602067dbac374bb5d3bd173cd5127ee0
yuvivian72 <107157533+yuvivian72@users.noreply.github.com>
2022-10-02 22:38:28 -0400

    Update lambda_function.py

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -25,8 +25,7 @@ def build_validation_result(is_valid, violated_slot, message_content):
         "violatedSlot": violated_slot,
         "message": {"contentType": "PlainText", "content": message_content},
     }
-
-
+    
 ### Dialog Actions Helper Functions ###
 def get_slots(intent_request):
     """
@@ -79,39 +78,6 @@ def close(session_attributes, fulfillment_state, message):
 
     return response
 
-
-"""
-Step 3: Enhance the Robo Advisor with an Amazon Lambda Function
-
-In this section, you will create an Amazon Lambda function that will validate the data provided by the user on the Robo Advisor.
-
-1. Start by creating a new Lambda function from scratch and name it `recommendPortfolio`. Select Python 3.7 as runtime.
-
-2. In the Lambda function code editor, continue by deleting the AWS generated default lines of code, then paste in the starter code provided in `lambda_function.py`.
-
-3. Complete the `recommend_portfolio()` function by adding these validation rules:
-
-    * The `age` should be greater than zero and less than 65.
-    * The `investment_amount` should be equal to or greater than 5000.
-
-4. Once the intent is fulfilled, the bot should respond with an investment recommendation based on the selected risk level as follows:
-
-    * **none:** "100% bonds (AGG), 0% equities (SPY)"
-    * **low:** "60% bonds (AGG), 40% equities (SPY)"
-    * **medium:** "40% bonds (AGG), 60% equities (SPY)"
-    * **high:** "20% bonds (AGG), 80% equities (SPY)"
-
-> **Hint:** Be creative while coding your solution, you can have all the code on the `recommend_portfolio()` function, or you can split the functionality across different functions, put your Python coding skills in action!
-
-5. Once you finish coding your Lambda function, test it using the sample test events provided for this Challenge.
-
-6. After successfully testing your code, open the Amazon Lex Console and navigate to the `recommendPortfolio` bot configuration, integrate your new Lambda function by selecting it in the “Lambda initialization and validation” and “Fulfillment” sections.
-
-7. Build your bot, and test it with valid and invalid data for the slots.
-
-"""
-
-
 ### Intents Handlers ###
 def recommend_portfolio(intent_request):
     """
@@ -124,9 +90,68 @@ def recommend_portfolio(intent_request):
     risk_level = get_slots(intent_request)["riskLevel"]
     source = intent_request["invocationSource"]
 
-    # YOUR CODE GOES HERE!
+    if source == "DialogCodeHook":
+        slots = get_slots(intent_request)
+        validation_result = validate_data(age, investment_amount, intent_request)
+        
+      
+        if not validation_result["isValid"]:
+            slots[validation_result["violatedSlot"]] = None
+            return elicit_slot(
+                intent_request["sessionAttributes"],
+                intent_request["currentIntent"]["name"],
+                slots,
+                validation_result["violatedSlot"],
+                validation_result["message"],
+            )
+            
+      
+        output_session_attributes = intent_request["sessionAttributes"]
+
+     
+        return delegate(output_session_attributes, get_slots(intent_request))
+    
+
+    risk_levels = {
+        'none': '100% bonds (AGG), 0% equities (SPY)',
+        'low': '60% bonds (AGG), 40% equities (SPY)',
+        'medium': '40% bonds (AGG), 60% equities (SPY)',
+        'high': '20% bonds (AGG), 80% equities (SPY)”'
+    }
+    
+    message = f"You should invest in {risk_levels.get(risk_level.lower())} for risk level {risk_level.lower()}."    
 
 
+    return close(
+        intent_request["sessionAttributes"],
+        "Fulfilled",
+        {
+            "contentType": "PlainText",
+            "content": message,
+        },
+    )
+
+def validate_data(age, investment_amount, intent_request):
+
+    if age is not None:
+        age = parse_int(age)
+        if age < 1 or age >= 65:
+            return build_validation_result(
+                False,
+                "age",
+                "Your age should be between 0 and 65 years old to use this service. Please provide a valid age.",
+            )
+
+    if investment_amount is not None:
+        dollars = parse_int(investment_amount)
+        if dollars < 5000:
+            return build_validation_result(
+                False,
+                "investmentAmount",
+                "The amount should be greater than or equal to 5000. Please provide a valid amount.",
+            )
+            
+    return build_validation_result(True, None, None)
 ### Intents Dispatcher ###
 def dispatch(intent_request):
     """
@@ -148,5 +173,4 @@ def lambda_handler(event, context):
     Route the incoming request based on intent.
     The JSON body of the request is provided in the event slot.
     """
-
     return dispatch(event)
